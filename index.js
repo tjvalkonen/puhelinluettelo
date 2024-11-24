12b3128029df7d3a8e11d342697bc5fa5c75db85
Tomas Valkonen <tomas.valkonen@helsinki.fi>
2024-11-24 03:02:12 +0200

    3.1-3.6.

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons  = [
+  { 
+    name: "Arto Hellas", 
+        number: "040-123456",
+        id: "1"
+  },
+  { 
+        name: "Ada Lovelace", 
+        number: "39-44-5323523",
+        id: "2"
+  },
+  { 
+        name: "Dan Abramov", 
+        number: "12-43-234345",
+        id: "3"
+  },
+  { 
+        name: "Mary Poppendieck", 
+        number: "39-23-6423122",
+        id: "4"
+  }
+]
+
+
+/*
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+*/
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    let l = persons.length
+    let date = new Date()
+    response.send(`<p>Phonebook has info for ${l} people</p></p>${date}</p>`)
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+
+  if(!person.name){
+    return response.status(400).json({ 
+      error: 'Name missing!' 
+    })
+  } else if (!person.number)
+  {
+    return response.status(400).json({ 
+      error: 'Number missing!' 
+    })
+  } else if (persons.find(p => p.name === person.name))  
+  {
+    return response.status(400).json({ 
+      error: `${person.name} already exists! Name must be unique.` 
+    })
+  } else {
+    const id = Math.floor(Math.random()*10000000)
+    person.id = String(id)
+    persons = persons.concat(person)
+    console.log(person)
+    response.json(person)
+  }
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
