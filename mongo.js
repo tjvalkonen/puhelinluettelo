79f92ba6e1efafe8f4153e65d0835dd02ec19c78
Tomas Valkonen <tomas.valkonen@helsinki.fi>
2024-11-26 04:03:38 +0200

    3.12.

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+const url = `mongodb+srv://tomasvalkonen:${password}@cluster0.8r4go.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(newName && newNumber){
+    const person = new Person({
+        name: newName,
+        number: newNumber,
+      })
+      
+      person.save().then(result => {
+        console.log(`added ${newName} number ${newNumber} to phonebook`)
+        mongoose.connection.close()
+      })
+} else if (!newName && !newNumber){
+    console.log('Phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
